41d44ea0ff29c84ac5d405a09a0a72b77ab536bd
ValeriiaM23 <mezencevavalerie@gmail.com>
2022-10-01 14:25:36 +0200

    doing the css and js

---
+++ src/index.js
@@ -0,0 +1,105 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import '../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+import fetchImages from './api-service'
+import markup from './templates';
+
+   
+const refs = {
+    form: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more')
+
+};
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+lightBox.on('show.lightbox');
+
+
+
+let currentPage = 1;
+let totalHits = 0;
+let query = '';
+
+const render = photos => {
+    const photoMarkUp = photos.map(markup).join('');
+    refs.gallery.insertAdjacentHTML('beforeend', photoMarkUp)
+};
+
+const clear = () => {
+    refs.gallery.innerHTML = '';
+    refs.loadMoreBtn.classList.add('is-hidden'); 
+    totalHits = 0;
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+
+async function onFormSubmit(event) {
+    event.preventDefault();
+    currentPage = 1;
+    query = event.target.elements.searchQuery.value.trim();
+    if (query === '') {
+        Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+        );
+        clear();
+        lightBox.refresh();
+        return;
+    }
+    const data = await fetchImages(query, currentPage);
+    totalHits = data.hits.length;
+
+
+    if (data.totalHits > 40) {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+    } else 
+    //  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    refs.loadMoreBtn.classList.add('is-hidden');
+
+    try {
+        if (data.totalHits === 0) {
+         Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+            );  
+            clear(); 
+        }
+        if (data.totalHits > 0) {
+            refs.gallery.innerHTML = '';
+            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+            render(data.hits, refs.gallery); 
+            lightBox.refresh(); 
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick)
+
+
+async function onLoadMoreBtnClick() {
+    currentPage += 1;
+    lightBox.refresh();
+    const response = await fetchImages(query, currentPage);
+    render(response.hits, refs.gallery);
+     totalHits += response.hits.length;
+    if (hits === response.totalHits) {
+       Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        ); 
+        refs.loadMoreBtn.classList.add('is-hidden');
+    }
+};
+
+
+
+
+
+
+
---
+++ src/templates.js
@@ -0,0 +1,31 @@
+export default function markup({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+}) {
+  return `<a class="photo-link" href="${largeImageURL}">
+    <div class="photo-card">
+    <img class=photo-img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+      </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>
+</a>
+    `
+}
+
